81efdf48517bc34673ce22e501620b9c3018bdc5
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2016-09-06 19:38:45 -0700

    add deepwater build ami scripts

---
+++ tools/provisioning/deepwater-build-ami/README.md
@@ -0,0 +1,5 @@
+# README
+
+This images are used to build binaries and release artifacts. 
+They are not meant to be used in a runtime environment.
+
---
+++ tools/provisioning/deepwater-build-ami/create_ami.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker run \
+  --rm \
+  --tty \
+  --interactive \
+  --volume $PWD:/workspace \
+  --workdir /workspace \
+  d11wtq/packer:0.8.5 \
+  packer build -debug \
+    -var source_ami=ami-2d39803a \
+    -var aws_region=us-east-1 \
+    -var version=1.7.1 \
+    -var revision=1 \
+    -var aws_access_key=$AWS_ACCESS_KEY_ID \
+    -var aws_secret_key=$AWS_SECRET_ACCESS_KEY \
+   $1 

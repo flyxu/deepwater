d945d7bfc6c35ad0fc89f476654e079e4151f04c
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2016-09-16 14:51:17 -0700

    fix mxnet + cuda build

--- backends/mxnet/tools/ci_build/ci_build.sh
+++ backends/mxnet/tools/ci_build/ci_build.sh
@@ -4,7 +4,8 @@
 git submodule update --init --recursive
 cd backends/mxnet/mxnet
 cp make/config.mk .
-export MAKEFLAGS="-j $(grep -c ^processor /proc/cpuinfo)"
-make 
+export MAKEFLAGS="-j $(grep -c ^processor /proc/cpuinfo) USE_CUDNN=1 USE_CUDA=1 USE_OPENCV=0 USE_CUDA_PATH=/usr/local/cuda"
+make clean
+make
 cd ..
-make 
+make
